01c8326931999f33d971d6b3ef85ae9d2b24d9d2
samira <houacine.samira@gmail.com>
2023-09-30 00:40:22 +0200

    porfolio

--- composants/Header.jsx
+++ composants/Header.jsx
@@ -60,6 +60,29 @@ export function Header() {
 
         <p> Montreuil 93100, Ile-de-France</p>
       </div>
+      <div className="uploadCV">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+          />
+        </svg>
+
+        <a
+          href="https://cvdesignr.com/p/64fd7eb8b5200"
+          download="Nom_du_CV.pdf"
+        >
+          Télécharger mon CV
+        </a>
+      </div>
     </div>
   );
 }
