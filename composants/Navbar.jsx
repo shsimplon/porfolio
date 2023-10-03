ac13a76a823d9731bbb8ae402f92aa1f177b1d26
samira <houacine.samira@gmail.com>
2023-10-03 11:56:40 +0200

    add menu burger

--- composants/Navbar.jsx
+++ composants/Navbar.jsx
@@ -2,21 +2,38 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeBurger = () => {
+    setIsBurgerOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="nav-list">
+      <div
+        className="burger-menu"
+        onClick={() => setIsBurgerOpen(!isBurgerOpen)}
+      >
+        ☰
+      </div>
+
+      <ul className={`nav-list ${isBurgerOpen ? "open" : ""}`}>
         <li>
           <a href="/">Accueil</a>
         </li>
         <li>
-          <a href="#about-me">À propos</a>
+          <a href="#about-me" onClick={closeBurger}>
+            À propos
+          </a>
         </li>
         <li>
-          <a href="#skills">Compétences</a>
+          <a href="#skills" onClick={closeBurger}>
+            Compétences
+          </a>
         </li>
         <li className="dropdown" onClick={() => setIsOpen(!isOpen)}>
           Plus
@@ -24,13 +41,19 @@ const Navbar = () => {
             <div className="overlay" onClick={toggleModal}>
               <ul className="dropdown-content">
                 <li>
-                  <a href="#experience">Expériences</a>
+                  <a href="#experience" onClick={closeBurger}>
+                    Expériences
+                  </a>
                 </li>
                 <li>
-                  <a href="#education">Éducation</a>
+                  <a href="#education" onClick={closeBurger}>
+                    Éducation
+                  </a>
                 </li>
                 <li>
-                  <a href="#contact">Contact</a>
+                  <a href="#contact" onClick={closeBurger}>
+                    Contact
+                  </a>
                 </li>
               </ul>
             </div>
